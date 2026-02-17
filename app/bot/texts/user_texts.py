79ba0f17295fed80057338766a91b32397667829
trensisflora <trensisflora@gmail.com>
2026-02-17 18:19:42 +0300

    Add Russian text messages for Telegram bot

---
+++ app/bot/texts/user_texts.py
@@ -0,0 +1,13 @@
+# User-facing Text Messages
+
+# Welcome message
+welcome_message = "Добро пожаловать в наш бот!"
+
+# Help message
+help_message = "Вы можете спросить меня о чем угодно. Я здесь, чтобы помочь!"
+
+# Error message
+error_message = "Упс! Что-то пошло не так. Пожалуйста, попробуйте еще раз."
+
+# Exit message
+exit_message = "Спасибо за использование нашего бота! Всего хорошего!"
